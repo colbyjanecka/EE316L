4cc827cc6e46d685cbb8b08cf88a9c14e6c265dd
Colby D Janecka <cjanecka@kamek.(none)>
2020-02-05 12:28:56 -0600

    started dataflow modeling for decoder

---
+++ Lab2/Lab2.srcs/sources_1/new/decoder.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Colby Janecka
+// 
+// Create Date: 02/05/2020 12:15:14 PM
+// Design Name: 
+// Module Name: decoder
+// Project Name: Lab2 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder(
+
+input up, left, right, enable,
+output combo0, combo1, combo2, combo3, combo4, combo5, combo6, combo7
+
+    );
+    
+// Structural
+
+// Dataflow
+
+assign combo0 = (~up) & (~left) & (~right) & (enable);
+assign combo1 = (~up) & (~left) & (right) & (enable);
+assign combo2 = (~up) & (left) & (~right) & (enable);
+
+// Behavioral    
+    
+endmodule
