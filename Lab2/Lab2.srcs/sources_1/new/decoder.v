9878e9ac84c7b93e548fd28d6d75905d091c75df
Colby D Janecka <cjanecka@bowser.ece.utexas.edu>
2020-02-13 16:06:16 -0600

    added structural modeling code

--- Lab2/Lab2.srcs/sources_1/new/decoder.v
+++ Lab2/Lab2.srcs/sources_1/new/decoder.v
@@ -22,24 +22,39 @@
 
 module decoder(
 
-input up, left, right, enable,
-output combo0, combo1, combo2, combo3, combo4, combo5, combo6, combo7
+    input up, left, right, enable,
+    output combo0, combo1, combo2, combo3, combo4, combo5, combo6, combo7
+//    output [0:7] combo
 
     );
     
-// Structural
-
-// Dataflow
-
-assign combo0 = (~up) & (~left) & (~right) & (enable);
-assign combo1 = (~up) & (~left) & (right) & (enable);
-assign combo2 = (~up) & (left) & (~right) & (enable);
-assign combo3 = (~up) & (left) & (right) & (enable);
-assign combo4 = (up) & (~left) & (~right) & (enable);
-assign combo5 = (up) & (~left) & (right) & (enable);
-assign combo6 = (up) & (left) & (~right) & (enable);
-assign combo7 = (up) & (left) & (right) & (enable);
-
-// Behavioral    
+    // Structural
+    
+    wire notUp, notLeft, notRight;
+    
+    not g1(notUp, up);
+    not g2(notLeft, left);
+    not g3(notRight, right);
+    and g4(combo0, notUp, notLeft, notRight, enable);
+    and g5(combo1, notUp, notLeft, right, enable);
+    and g6(combo2, notUp, left, notRight, enable);
+    and g7(combo3, notUp, left, right, enable);
+    and g8(combo4, up, notLeft, notRight, enable);
+    and g9(combo5, up, notLeft, right, enable);
+    and g10(combo6, up, left, notRight, enable);
+    and g11(combo7, up, left, right, enable);
+    
+    
+    // Dataflow
+//    assign combo0 = (~up) & (~left) & (~right) & (enable);
+//    assign combo1 = (~up) & (~left) & (right) & (enable);
+//    assign combo2 = (~up) & (left) & (~right) & (enable);
+//    assign combo3 = (~up) & (left) & (right) & (enable);
+//    assign combo4 = (up) & (~left) & (~right) & (enable);
+//    assign combo5 = (up) & (~left) & (right) & (enable);
+//    assign combo6 = (up) & (left) & (~right) & (enable);
+//    assign combo7 = (up) & (left) & (right) & (enable);
+    
+    // Behavioral    
     
 endmodule
