59b538cb713ad0e49383007bf4722464cdf9de96
colbyjanecka <cjanecka@kamek.(none)>
2020-02-28 12:27:48 -0600

    management

---
+++ Lab3/Lab3_BCD-Converter/Lab3_BCD-Converter.srcs/sim_1/new/tb_bcd-converter.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/28/2020 12:12:54 PM
+// Design Name: 
+// Module Name: tb_bcd-converter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_bcd-converter;
+
+
+reg s0, s1, left, right, up, down;
+wire out;
+
+    // Module test instantiation
+    mux m(.up(up), .left(left), .right(right), .down(down),
+     .s0(s0), .s1(s1), .out(out));
+    
+    initial begin
+    up=0; left=0; right=0; down= 0; s0=0; s1=0;     // up selected, not pressed   
+    #10                        
+    up=1; left=0; right=0; down= 0; s0=0; s1=0;     // up selected, pressed       
+    #10  
+    up=0; left=0; right=0; down= 0; s0=0; s1=1;     // left selected, not pressed       
+    #10     
+    up=0; left=1; right=0; down= 0; s0=0; s1=1;     // left selected, pressed       
+    #10
+    up=0; left=0; right=0; down= 0; s0=1; s1=0;     // right selected, not pressed       
+    #10                        
+    up=0; left=0; right=1; down= 0; s0=1; s1=0;     // right selected, pressed       
+    #10
+    up=0; left=0; right=0; down= 0; s0=1; s1=1;     // down selected, not pressed       
+    #10                        
+    up=0; left=0; right=0; down= 1; s0=1; s1=1;     // down selected, pressed       
+    #10;                       
+    end
+
+
+
+endmodule
---
+++ Lab3/Lab3_BCD-Converter/Lab3_BCD-Converter.srcs/sources_1/new/bcd-converter.v
@@ -0,0 +1,70 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Colby Janecka
+// 
+// Create Date: 02/05/2020 12:15:14 PM
+// Design Name: 
+// Module Name: decoder
+// Project Name: Lab2 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux(
+
+    input s0, s1, up, left, right, down,
+    output out
+
+    );
+    
+    // Structural
+    
+    wire s0not, s1not; //s0 not, s1 not
+    wire upwire, leftwire, rightwire, downwire;
+  
+    not g1(s0not, s0);
+    not g2(s1not, s1);
+  
+    and g3(upwire, up, s1not, s0not);
+    and g4(leftwire, left, s1not, s0);
+    and g5(rightwire, right, s1,s0not);
+    and g6(downwire, down, s1,s0);
+  
+    or g7(out, upwire, leftwire, rightwire, downwire);
+    
+    // Dataflow
+
+//    assign out = s1 ? (s0 ? down : right) : (s0 ? left : up); 
+    
+    
+    // Behavioral
+    
+//    reg out_buf = 0;
+//    assign out = out_buf;
+    
+    
+//    always @ (up or left or right or down or s0 or s1) 
+//    begin
+//        case ({s1, s0})
+//            2'b00: out_buf <= up;
+//            2'b01: out_buf <= left;
+//            2'b10: out_buf <= right;
+//            2'b11: out_buf <= down;
+//        endcase
+//    end
+
+
+        
+    
+endmodule
+
