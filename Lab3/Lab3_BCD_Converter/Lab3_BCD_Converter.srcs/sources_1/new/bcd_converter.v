31a228746d02a4c1de97ec7b796e1548823f0488
colbyjanecka <cjanecka@kamek.(none)>
2020-02-28 15:38:35 -0600

    final behavioural design

--- Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
+++ Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
@@ -76,21 +76,21 @@ assign dp = 1; // Disable decimal point since we aren't using it in this lab
     begin
         case(sw)
         4'b0000: seg_buf = 7'b1000000;
-        4'b0001: seg_buf = 7'b1001111;
-        4'b0010: seg_buf = 7'b0010010;
-        4'b0011: seg_buf = 7'b0000110;
-        4'b0100: seg_buf = 7'b1001100;
-        4'b0101: seg_buf = 7'b0100100;
-        4'b0110: seg_buf = 7'b0100000;
-        4'b0111: seg_buf = 7'b0001111;
+        4'b0001: seg_buf = 7'b1111001;
+        4'b0010: seg_buf = 7'b0100100;
+        4'b0011: seg_buf = 7'b0110000;
+        4'b0100: seg_buf = 7'b0011001;
+        4'b0101: seg_buf = 7'b0010010;
+        4'b0110: seg_buf = 7'b0000010;
+        4'b0111: seg_buf = 7'b1111000;
         4'b1000: seg_buf = 7'b0000000;
-        4'b1001: seg_buf = 7'b0000100;
+        4'b1001: seg_buf = 7'b0010000;
         4'b1010: seg_buf = 7'b0001000;
-        4'b1011: seg_buf = 7'b1100000;
-        4'b1100: seg_buf = 7'b0110001;
-        4'b1101: seg_buf = 7'b1000010;
-        4'b1110: seg_buf = 7'b0110000;
-        4'b1111: seg_buf = 7'b0111000;
+        4'b1011: seg_buf = 7'b0000011;
+        4'b1100: seg_buf = 7'b1000110;
+        4'b1101: seg_buf = 7'b0100001;
+        4'b1110: seg_buf = 7'b0000110;
+        4'b1111: seg_buf = 7'b0001110;
     
 
         endcase
