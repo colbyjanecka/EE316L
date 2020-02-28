ace941d5324c548f0691204568b6fbbe67768e38
colbyjanecka <cjanecka@kamek.(none)>
2020-02-28 15:01:05 -0600

    Dataflow BCD

--- Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
+++ Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
@@ -35,10 +35,36 @@ assign dp = 1; // Disable decimal point since we aren't using it in this lab
 
     // Dataflow
 
-//    assign seg[0] = sw[0];
+    assign seg[0] = (~sw[3] & ~sw[2] & ~sw[1] & sw[0]) | 
+                    (~sw[3] & sw[2] & ~sw[1] & ~sw[0]) |
+                    (sw[3] & sw[2] & ~sw[1] & sw[0]) |
+                    (sw[3] & ~sw[2] & ~sw[1] & sw[0]);
+                    
+    assign seg[1] = (~sw[3] & sw[2] & ~sw[1] & sw[0]) | 
+                    (sw[2] & sw[1] & ~sw[0]) |
+                    (sw[3] & sw[2] & ~sw[1] & ~sw[0]) |
+                    (sw[3] & sw[1] & sw[0]);
+                    
+    assign seg[2] = (~sw[3] & ~sw[2] & sw[1] & ~sw[0]) | 
+                    (sw[3] & sw[2] & sw[1]) |
+                    (sw[3] & sw[2] & ~sw[0]);
+                    
+    assign seg[3] = (~sw[3] & ~sw[2] & ~sw[1] & sw[0]) | 
+                    (~sw[3] & sw[2] & ~sw[1] & ~sw[0]) |
+                    (sw[2] & sw[1] & sw[0]) |
+                    (sw[3] & ~sw[2] & sw[1] & ~sw[0]);
+                    
     assign seg[4] = (~sw[3] & sw[0]) | (~sw[3] & sw[2] & ~sw[1])
                     |(sw[0] & ~sw[1] & ~sw[2]);
-
+                    
+    assign seg[5] = (~sw[3] & ~sw[2] & sw[0]) | 
+                    (~sw[3] & ~sw[2] & sw[1]) |
+                    (~sw[3] & sw[1] & sw[0]) |
+                    (sw[3] & sw[2] & ~sw[1] & sw[0]); 
+                    
+    assign seg[6] = (~sw[3] & ~sw[2] & ~sw[1]) | 
+                    (~sw[3] & sw[2] & sw[1] & sw[0]) |
+                    (sw[3] & sw[2] & ~sw[1] & ~sw[0]);                
 
     // Behavioral
 
