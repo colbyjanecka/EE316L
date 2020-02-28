ab6f59772e500b8246edb52a76b0c0717abe2318
colbyjanecka <cjanecka@kamek.(none)>
2020-02-28 16:27:01 -0600

    hopefully last commit for structural

--- Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
+++ Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
@@ -122,33 +122,33 @@ assign dp = 1; // Disable decimal point since we aren't using it in this lab
 
     // Behavioral
 
-    reg [6:0] seg_buf = 0;
-    assign seg = seg_buf;
+//    reg [6:0] seg_buf = 0;
+//    assign seg = seg_buf;
 
 
-    always @(*)
-    begin
-        case(sw)
-        4'b0000: seg_buf = 7'b1000000;
-        4'b0001: seg_buf = 7'b1111001;
-        4'b0010: seg_buf = 7'b0100100;
-        4'b0011: seg_buf = 7'b0110000;
-        4'b0100: seg_buf = 7'b0011001;
-        4'b0101: seg_buf = 7'b0010010;
-        4'b0110: seg_buf = 7'b0000010;
-        4'b0111: seg_buf = 7'b1111000;
-        4'b1000: seg_buf = 7'b0000000;
-        4'b1001: seg_buf = 7'b0010000;
-        4'b1010: seg_buf = 7'b0001000;
-        4'b1011: seg_buf = 7'b0000011;
-        4'b1100: seg_buf = 7'b1000110;
-        4'b1101: seg_buf = 7'b0100001;
-        4'b1110: seg_buf = 7'b0000110;
-        4'b1111: seg_buf = 7'b0001110;
+//    always @(*)
+//    begin
+//        case(sw)
+//        4'b0000: seg_buf = 7'b1000000;
+//        4'b0001: seg_buf = 7'b1111001;
+//        4'b0010: seg_buf = 7'b0100100;
+//        4'b0011: seg_buf = 7'b0110000;
+//        4'b0100: seg_buf = 7'b0011001;
+//        4'b0101: seg_buf = 7'b0010010;
+//        4'b0110: seg_buf = 7'b0000010;
+//        4'b0111: seg_buf = 7'b1111000;
+//        4'b1000: seg_buf = 7'b0000000;
+//        4'b1001: seg_buf = 7'b0010000;
+//        4'b1010: seg_buf = 7'b0001000;
+//        4'b1011: seg_buf = 7'b0000011;
+//        4'b1100: seg_buf = 7'b1000110;
+//        4'b1101: seg_buf = 7'b0100001;
+//        4'b1110: seg_buf = 7'b0000110;
+//        4'b1111: seg_buf = 7'b0001110;
 
 
-        endcase
-    end
+//        endcase
+//    end
 
 
 
