37845d8786f655b5ee37b50615a275ef61595954
colbyjanecka <cjanecka@kamek.(none)>
2020-02-28 16:04:21 -0600

    switching to atom

--- Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
+++ Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
@@ -31,7 +31,36 @@ assign dp = 1; // Disable decimal point since we aren't using it in this lab
 
     // Structural
 
-
+    wire wireA0, wireA1, wireA2, wireA3, 
+         wireB0, wireB1, wireB2, wireB3,
+         wireC0, wireC1, wireC2,
+         wireD0, wireD1, wireD2, wireD3,
+         wireE0, wireE1, wireE2,
+         wireF0, wireF1, wireF2, wireF3,
+         wireG0, wireG1, wireG2;
+         
+    wire not0, not1, not2, not3;
+    
+    not n0(not0, sw[0]);
+    not n1(not1, sw[1]);
+    not n2(not2, sw[2]);
+    not n3(not3, sw[3]);
+    
+    and andA0(wireA0, not3, not3, not1, sw[0]);
+    and andA1(wireA1, not3, sw[2], not1, not0);
+    and andA2(wireA2, sw[3], sw[2], not1, sw[0]);
+    and andA3(wireA3, sw[3], not2, sw[1], sw[0]);
+    
+    and andB0(wireB0, not3, sw[2], not1, sw[0]);
+    and andB1(wireB1, sw[2], sw[1], not0);
+    and andB2(wireB2, sw[3], sw[2], not1, not0);
+    and andB3(wireB3, sw[3], sw[1], sw[0]);
+    
+    
+    
+    
+    and andC(wireC, 
+    
 
     // Dataflow
 
