315063a9573a47ec475e5c9024a4515541d67c96
colbyjanecka <cjanecka@kamek.(none)>
2020-02-28 14:27:48 -0600

    .

--- Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
+++ Lab3/Lab3_BCD_Converter/Lab3_BCD_Converter.srcs/sources_1/new/bcd_converter.v
@@ -36,9 +36,8 @@ assign dp = 1; // Disable decimal point since we aren't using it in this lab
     // Dataflow
 
 //    assign seg[0] = sw[0];
-    assign seg[0] = (sw[3] & ~sw[1]) | (sw[3] & ~sw[2] & ~sw[0])
-                    |(~sw[2] & ~sw[1]) | (sw[2] & ~sw[0])
-                    |(~sw[3] & sw[0]) | (~sw[3] & sw[2] & sw[1]);
+    assign seg[4] = (~sw[3] & sw[0]) | (~sw[3] & sw[2] & ~sw[1])
+                    |(sw[0] & ~sw[1] & ~sw[2]);
 
 
     // Behavioral
