98409ecbcb329a820fa24945f98f56e26df08d62
Colby Janecka <colbyjanecka@aol.com>
2020-04-05 15:44:08 -0500

    created lab4 project

---
+++ Lab4/Lab4_PartA/Lab4_PartA.srcs/sources_1/new/clks.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/05/2020 03:38:44 PM
+// Design Name: 
+// Module Name: clks
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clks(
+    input clk,
+    output [15:0] led
+    );
+    
+    
+    
+endmodule
