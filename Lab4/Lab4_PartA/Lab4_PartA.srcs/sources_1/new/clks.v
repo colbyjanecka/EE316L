b786e482a871ae83c454bbfce1babec70a8082c1
colbyjanecka <colbyjanecka@aol.com>
2020-04-05 16:25:52 -0500

    added clocks for leds 1-11

--- Lab4/Lab4_PartA/Lab4_PartA.srcs/sources_1/new/clks.v
+++ Lab4/Lab4_PartA/Lab4_PartA.srcs/sources_1/new/clks.v
@@ -27,8 +27,19 @@ module clks(
 
     wire div2;
     reg [11:0] count = 0;
-    
-    assign led[0] = count[0];   // set led to 50 hz
+
+    assign led[0] = count[0];   // set led to 50 MHz
+    assign led[1] = count[1];   // set led to 25 MHz
+    assign led[2] = count[2];   // set led to 12.5 MHz
+    assign led[3] = count[3];   // set led to 6.25 MHz
+    assign led[4] = count[4];   // set led to 3.13 MHz
+    assign led[5] = count[5];   // set led to 1.56 MHz
+    assign led[6] = count[6];   // set led to 0.78 MHz
+    assign led[7] = count[7];   // set led to 0.39 MHz
+    assign led[8] = count[8];   // set led to 195 kHz
+    assign led[9] = count[9];   // set led to 97.7 kHz
+    assign led[10] = count[10];   // set led to 48.8 kHz
+    assign led[11] = count[11];   // set led to 24.4 kHz
 
     always @(posedge clk) begin
         count = count + 1;
