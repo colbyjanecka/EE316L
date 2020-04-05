8e6e9f5672167c5df4a2b868e03ea7aac63e934b
colbyjanecka <colbyjanecka@aol.com>
2020-04-05 18:00:32 -0500

    led 15

--- Lab4/Lab4_PartA/Lab4_PartA.srcs/sources_1/new/clks.v
+++ Lab4/Lab4_PartA/Lab4_PartA.srcs/sources_1/new/clks.v
@@ -38,25 +38,29 @@ module clks(
     assign div13 = count13[6] & count13[5] & count13[1] & count13[0];
     assign led[13] = div13;
 
-    reg [10:0] count14 = 0;
+    reg [9:0] count14 = 0;
     assign div14 = count14[9] & count14[8] & count14[7] & count14[6]
                 & count14[5] & count14[2] & count14[1] & count14[0];
     assign led[14] = div14;
 
-    assign div2 = count[0];
-    assign led[15] = div2;   // 50 MHz placeholder
+    reg[13:0] count15 = 0;
+    assign div15 = count15[13] & count15[10] & count15[9] & count15[8]
+                & count15[3] & count15[2] & count15[1] & count15[0];
 
     always @(posedge clk) begin
         count = count + 1;  // count for led 0 - 11
 
-        if(count12 == 9) count12 = 0;
+        if(count12 == 9) count12 = 0; // count for led 12
         else count12 = count12 + 1;
 
-        if(count13 == 99) count13 = 0;
+        if(count13 == 99) count13 = 0; // count for led 13
         else count13 = count13 + 1;
 
-        if(count14 == 999) count14 = 0;
+        if(count14 == 999) count14 = 0; // count for led 14
         else count14 = count14 + 1;
+
+        if(count15 == 9999) count15 = 0; // count for led 15
+        else count15 = count15 + 1;
 
     end
 
