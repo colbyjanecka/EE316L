1791a6583234ffb323c48d011797e06f58261004
colbyjanecka <colbyjanecka@aol.com>
2020-04-20 15:29:06 -0500

    m

---
+++ Lab5/Lab5_PartA/Lab5_PartA.srcs/sources_1/new/datapath.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2020 03:26:53 PM
+// Design Name: 
+// Module Name: datapath
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module datapath(
+
+    );
+endmodule
