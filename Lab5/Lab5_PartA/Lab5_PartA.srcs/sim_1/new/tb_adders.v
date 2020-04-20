d62ceadd4e5472641414b7bd22ce9b6d2e8ee68e
colbyjanecka <colbyjanecka@aol.com>
2020-04-20 12:45:30 -0500

    getter

---
+++ Lab5/Lab5_PartA/Lab5_PartA.srcs/sim_1/new/tb_adders.v
@@ -0,0 +1,97 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/17/2020 03:10:43 PM
+// Design Name: 
+// Module Name: tb_adders
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_adders;
+
+reg clk;
+reg load;
+reg[3:0] a;
+reg[3:0] b;
+reg Cin;
+wire[4:0] total;
+
+rca uut (
+    .clk(clk),    
+    .load(load),
+    .a(a),
+    .b(b),
+    .Cin(Cin),
+    .total(total)
+);
+
+initial begin
+
+clk = 0;
+load = 0;
+
+a = 4'b0001;
+b = 4'b0101;
+Cin = 1'b0;
+
+#50;
+
+load = 1;
+
+#50;
+
+a = 4'b0111;
+b = 4'b0111;
+Cin = 1'b0;
+
+#50;
+
+a = 4'b1000;
+b = 4'b0111;
+Cin = 1'b1;
+
+#50;
+
+a = 4'b1100;
+b = 4'b0100;
+Cin = 1'b0;
+
+#50;
+
+a = 4'b1000;
+b = 4'b1000;
+Cin = 1'b1;
+
+#50;
+
+a = 4'b1001;
+b = 4'b1010;
+Cin = 1'b1;
+
+#50;
+
+a = 4'b1111;
+b = 4'b1111;
+Cin = 1'b0;
+
+#50;
+
+end
+
+always
+#1 clk = ~clk;
+
+endmodule
---
+++ Lab5/Lab5_PartA/Lab5_PartA.srcs/sources_1/new/rca.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/17/2020 02:31:24 PM
+// Design Name: 
+// Module Name: rca
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rca(
+input clk, load,
+ input [3:0] a, b,
+ input Cin, 
+ output [4:0] total); 
+    
+wire s0, s1, s2, s3, o0, o1, o2, o3;
+reg[4:0] D;
+
+adder c1 (.a(a[0]), .b(b[0]), .Cin(Cin), .S(s0), .Cout(o0));
+adder c2 (.a(a[1]), .b(b[1]), .Cin(o0), .S(s1), .Cout(o1));
+adder c3 (.a(a[2]), .b(b[2]), .Cin(o1), .S(s2), .Cout(o2));
+adder c4 (.a(a[3]), .b(b[3]), .Cin(o2), .S(s3), .Cout(o3));
+
+always @(*) begin
+    D = {o3, s3, s2, s1, s0};
+end
+
+loadreg c5 (.clk(clk), .load(load), .D(D), .Q(total)); 
+    
+endmodule
