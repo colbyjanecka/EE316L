5c030845ccac0436d7518c76f9bd7780b6e2621b
colbyjanecka <colbyjanecka@aol.com>
2020-04-20 15:24:10 -0500

    eh

--- Lab5/Lab5_PartA/Lab5_PartA.srcs/sources_1/new/cla.v
+++ Lab5/Lab5_PartA/Lab5_PartA.srcs/sources_1/new/cla.v
@@ -24,27 +24,36 @@ module cla(
     input clk, load,
     input [3:0] a, b,
     input Cin,
-    output [4:0] total
-    );
-
-    wire [3:0] G, P, C;
-
-    wire [4:0] S;
+    output [4:0] total);
+    
+    wire [3:0] G, P, S;
+    wire [4:0] C;
     wire Cout;
-    wire [4:0] Data;
-
-    assign Q = {Cout, S};
-
-    assign P = a ^ b;
-    assign G = a&b;
-    assign C = P ^ S;
-    assign S = G + (P & G);
-
-
-    //assign Data = {Cout, S};
-    assign Data[4] = Cout;
-    assign Data[3:0] = C;
-
-    loadreg reg(.clk(clk), .load(load), .Data(Data), .total(total));
+    wire [4:0] D;
+    
+    //CLA Dataflow Model
+    assign P[0] = a[0] ^ b[0];
+    assign P[1] = a[1] ^ b[1];
+    assign P[2] = a[2] ^ b[2];
+    assign P[3] = a[3] ^ b[3];
+    assign G[0] = a[0] & b[0];
+    assign G[1] = a[1] & b[1];
+    assign G[2] = a[2] & b[2];
+    assign G[3] = a[3] & b[3];
+    assign C[0] = Cin;
+    assign C[1] = G[0] | (P[0]&C[0]);
+    assign C[2] = (P[1]&P[0]&C[0]) | (P[1]&G[0]) | G[1];
+    assign C[3] = (P[2]&P[1]&P[0]&C[0]) | (P[2]&P[1]&G[0]) | (P[2]&G[1]) | G[2];
+    assign C[4] = (P[3]&P[2]&P[1]&P[0]&C[0]) | (P[3]&P[2]&P[1]&G[0]) | (P[3]&P[2]&G[1]) | (P[3]&G[2]) | G[3];
+    assign Cout = C[4];
+    
+    assign S[0] = P[0] ^ C[0];
+    assign S[1] = P[1] ^ C[1];
+    assign S[2] = P[2] ^ C[2];
+    assign S[3] = P[3] ^ C[3];
+    
+    assign D = {Cout, S};
+    
+    loadreg r0(.clk(clk), .load(load), .D(D), .Q(total));
 
 endmodule
